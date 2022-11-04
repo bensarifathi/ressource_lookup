6049490c6f5fd488923e15240b277dbc9090c1de
fathibensari <fethibensari@gmail.com>
2022-11-04 22:41:56 +0100

    remove unnecessary boilerplate

--- books/views.py
+++ books/views.py
@@ -6,12 +6,5 @@ from .models import Book
 class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
-
-    def get_queryset(self):
-        fields : str = self.request.query_params.get('fields')
-        fields_list = fields.split(',')
-        if not fields:
-            return super().get_queryset()
-        return Book.objects.only('id')
         
         
